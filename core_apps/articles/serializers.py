c0c0af3137b88d376d7c21940babbf66799643be
Tong Xia <gregxia0625@gmail.com>
2024-01-16 16:38:47 -0600

    feat: add and remove article bookmarks

--- core_apps/articles/serializers.py
+++ core_apps/articles/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from core_apps.articles.models import Article, ArticleView
 from core_apps.profiles.serializers import ProfileSerializer
+from core_apps.bookmarks.models import Bookmark
+from core_apps.bookmarks.serializers import BookmarkSerializer
 
 class TagListField(serializers.Field):
     def to_representation(self, value):
@@ -26,10 +28,19 @@ class ArticleSerializer(serializers.ModelSerializer):
     banner_image = serializers.SerializerMethodField()
     estimated_reading_time = serializers.ReadOnlyField()
     tags = TagListField()
+    bookmarks = serializers.SerializerMethodField()
+    bookmarks_count = serializers.SerializerMethodField()
     views = serializers.SerializerMethodField()
     created_at = serializers.SerializerMethodField()
     updated_at = serializers.SerializerMethodField()
     average_rating = serializers.ReadOnlyField()
+
+    def get_bookmarks(self, obj):
+        bookmarks = Bookmark.objects.filter(article=obj)
+        return BookmarkSerializer(bookmarks, many=True).data
+
+    def get_bookmarks_count(self, obj):
+        return Bookmark.objects.filter(article=obj).count()
 
     def get_average_rating(self, obj):
         return obj.average_rating()
@@ -85,6 +96,8 @@ class ArticleSerializer(serializers.ModelSerializer):
             "description",
             "body",
             "banner_image",
+            "bookmarks_count",
+            "bookmarks",
             "created_at",
             "updated_at",
             "average_rating",
--- core_apps/bookmarks/models.py
+++ core_apps/bookmarks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from core_apps.articles.models import Article
 
-# Create your models here.
+User = get_user_model()
+
+class Bookmark(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "bookmarks")
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="bookmarks")
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        unique_together = ["user", "article"]
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.user.first_name} bookmarked {self.article.title}"
